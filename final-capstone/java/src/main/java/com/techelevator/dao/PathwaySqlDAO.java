d11a98c8462cf14b1def98d70035b3ecaa7b7118
Vilasini Sarveswara <vilasini.s@gmail.com>
2021-05-28 14:59:18 -0400

    Backend fixed

--- final-capstone/java/src/main/java/com/techelevator/dao/PathwaySqlDAO.java
+++ final-capstone/java/src/main/java/com/techelevator/dao/PathwaySqlDAO.java
@@ -24,104 +24,106 @@ public class PathwaySqlDAO implements PathwayDAO {
     @Override
     public Response getPathwayDetails(String topic) {
         Response response = new Response();
-        List<String> responseList = new ArrayList<>();
-        List<String> responseLinkList = new ArrayList<>();
         if (topic.equalsIgnoreCase("technical")) {
-
-
-            String sql = "SELECT response, responselinks FROM technical_questions;";
-
-            SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
-
-            int responseCounter = 0;
-            while(results.next()) {
-               responseCounter++;
-               if( responseCounter < 17) {
-                   String responseText = mapToResponse(results);
-                   responseList.add(responseText);
-               }
-               else {
-                   String responseLink = mapToResponseLink(results);
-                   responseLinkList.add(responseLink);
-               }
-               response.setResponseTextList(responseList);
-               response.setResponseLinkList(responseLinkList);
-                return response;
-            }
-
+            response = fetchTechnicalResults();
+            return response;
         } else if (topic.equalsIgnoreCase("cover")) {
-
-            int id = (int) ((Math.random() * 7) + 1);
-
-
-            String sql = "SELECT response FROM cover_letter WHERE id = ?;";
-
-            SqlRowSet result = jdbcTemplate.queryForRowSet(sql, id);
-
-            if (result.next()) {
-                String responseLink = result.getString("response");
-
-//                Response response = new Response();
-//                response.setResponseLink(responseLink);
-                return response;
-            }
+            response =fetchCoverResults();
+            return response;
 
         } else if (topic.equalsIgnoreCase("business_attire")) {
-
-            int id = (int) ((Math.random() * 3) + 1);
-
-
-            String sql = "SELECT response FROM business_attire WHERE id = ?;";
-
-            SqlRowSet result = jdbcTemplate.queryForRowSet(sql, id);
-
-            if (result.next()) {
-                String responseLink = result.getString("response");
-
-//                Response response = new Response();
-//                response.setResponseLink(responseLink);
+            response = fetchAttireResults();
                 return response;
-            }
-
 
         } else if (topic.equalsIgnoreCase("behavioral")) {
-
-            int id = (int) ((Math.random() * 19) + 1);
-
-
-            String sql = "SELECT response FROM behavioral_questions WHERE id = ?;";
-
-            SqlRowSet result = jdbcTemplate.queryForRowSet(sql, id);
-
-            if (result.next()) {
-                String responseText = result.getString("response");
-
-//                Response response = new Response();
-//                response.setResponseText(responseText);
-                return response;
-            }
-
-
-        } else if (topic.equalsIgnoreCase("general")) {
-
-            String sql = "SELECT * FROM pathway_general";
-
-            SqlRowSet result = jdbcTemplate.queryForRowSet(sql);
-
-            if (result.next()) {
-                String responseLink = result.getString("response");
-
-//                Response response = new Response();
-//                response.setResponseLink(responseLink);
-                return response;
-            }
-
-
+            response = fetchBehavioralResults();
+            return response;
         }
 
-
-        return null;
+         else if (topic.equalsIgnoreCase("general")) {
+             response = fetchGeneralResults();
+             return response;
+        }
+         return null;
     }
+
+    public Response fetchTechnicalResults(){
+        Response response = new Response();
+        List<String> responseList = new ArrayList<>();
+        List<String> responseLinkList = new ArrayList<>();
+        String sql = "SELECT response, responselinks FROM technical_questions;";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+        while(results.next()){
+            String responseText = mapToResponse(results);
+            responseList.add(responseText);
+            if(!(results.getString("responselinks") == null)){
+                String responseLink = mapToResponseLink(results);
+                responseLinkList.add(responseLink);
+            }
+        }
+        response.setResponseTextList(responseList);
+        response.setResponseLinkList(responseLinkList);
+        return response;
+    }
+
+    public Response fetchBehavioralResults(){
+        Response behavioralResponse = new Response();
+        List<String> responseList = new ArrayList<>();
+        List<String> responseLinkList = new ArrayList<>();
+        String sql = "SELECT response, responselinks FROM behavioral_questions;";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+        while(results.next()){
+            String responseText = mapToResponse(results);
+            responseList.add(responseText);
+            if(!(results.getString("responselinks") == null)){
+                String responseLink = mapToResponseLink(results);
+                responseLinkList.add(responseLink);
+            }
+        }
+        behavioralResponse.setResponseTextList(responseList);
+        behavioralResponse.setResponseLinkList(responseLinkList);
+        return behavioralResponse;
+    }
+
+    public Response fetchCoverResults() {
+        Response coverResponse = new Response();
+        List<String> responseLinkList = new ArrayList<>();
+        String sql = "SELECT response FROM cover_letter ;";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+        while (results.next()) {
+            String responseLink = results.getString("response");
+            responseLinkList.add(responseLink);
+
+        }
+        coverResponse.setResponseLinkList(responseLinkList);
+        return coverResponse;
+    }
+    public Response fetchAttireResults() {
+        Response coverResponse = new Response();
+        List<String> responseLinkList = new ArrayList<>();
+
+        String sql = "SELECT response FROM business_attire;";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+        while (results.next()) {
+            String responseLink = results.getString("response");
+            responseLinkList.add(responseLink);
+        }
+        coverResponse.setResponseLinkList(responseLinkList);
+        return coverResponse;
+    }
+    public Response fetchGeneralResults(){
+        Response coverResponse = new Response();
+        List<String> responseLinkList = new ArrayList<>();
+        String sql = "SELECT * FROM pathway_general";
+        SqlRowSet result = jdbcTemplate.queryForRowSet(sql);
+        if (result.next()) {
+            String responseLink = result.getString("response");
+            responseLinkList.add(responseLink);
+        }
+        coverResponse.setResponseLinkList(responseLinkList);
+        return coverResponse;
+    }
+
 
     // helper method
     public String mapToResponse(SqlRowSet results) {
@@ -129,6 +131,7 @@ public class PathwaySqlDAO implements PathwayDAO {
         return response;
     }
     public String mapToResponseLink(SqlRowSet results) {
+
         String responseLink = results.getString("responseLinks");
         return responseLink;
     }
