7d310f408406a02f8a3a1c8b3f007d349ca9f855
Vilasini Sarveswara <vilasini.s@gmail.com>
2021-05-30 12:46:31 -0400

    Implemented quote & weather API in the backend

---
+++ final-capstone/java/src/main/java/com/techelevator/model/JobSearch.java
@@ -0,0 +1,40 @@
+package com.techelevator.model;
+
+public class JobSearch {
+    String title;
+    String location;
+    String jobDescription;
+    String companyName;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getJobDescription() {
+        return jobDescription;
+    }
+
+    public void setJobDescription(String jobDescription) {
+        this.jobDescription = jobDescription;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+}
